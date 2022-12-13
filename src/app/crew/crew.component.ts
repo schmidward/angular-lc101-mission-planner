4856597a6f95d6f902dd3a460af15a98e8fe1690
schmidward <eric.d.schmid1@gmail.com>
2022-12-12 19:40:02 -0600

    studio finished

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,10 +12,29 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  memberBeingEdited: object = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(name: string, isFirst: boolean): void {
+    this.crew.push({name: name, firstMission: isFirst});
+  }
+
+  remove(member: object): void {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(updatedName: string, member: object): void {
+    member['name'] = updatedName;
+    this.memberBeingEdited = null;
   }
 
 }
