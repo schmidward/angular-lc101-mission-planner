a126d744b7e6f01842a9396e8ae1dee8e3f50156
schmidward <eric.d.schmid1@gmail.com>
2022-12-15 20:16:09 -0600

    double blah

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,12 +25,19 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-  addCrewMember(person: object): void {
-    if (this.crew.length < 3 && !this.crew.includes(person)) {
-      this.crew.push(person);
+  addCrewMember(member: object) {
+    if (this.crew.includes(member)) {
+      this.inCrew = true;
+    } else {
+      this.inCrew = false;
     }
-    if (this.crew.includes(person)){
-      this.crew.splice();
+    
+    if (this.inCrew) {
+        let index = this.crew.indexOf(member);
+        this.crew.splice(index, 1);
+    } else if (!this.inCrew && this.crew.length < 3) {
+        this.crew.push(member);
     }
   }
+
 }
